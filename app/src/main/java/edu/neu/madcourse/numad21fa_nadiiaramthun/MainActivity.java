f994f40bab6ff2a0d33c4f49fbc99f297c869ca5
ndamrina <ramthun.n@northeastern.edu>
2021-09-12 08:50:16 -0700

    Added onClick for About button

--- app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/MainActivity.java
@@ -3,6 +3,8 @@ package edu.neu.madcourse.numad21fa_nadiiaramthun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAboutBtnClick(View view) {
+        Toast.makeText(view.getContext(),
+                "Nadiia Ramthun\nramthun.n@northeastern.edu",
+                Toast.LENGTH_LONG)
+                .show();
     }
 }
