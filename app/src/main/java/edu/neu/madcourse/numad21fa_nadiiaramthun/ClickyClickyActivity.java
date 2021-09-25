3bb9258bb36d070c850fa2f9ad87783737c68a96
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-09-25 12:31:23 -0700

    Added onTouch events for the buttons in ClickyClicky

--- app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/ClickyClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/ClickyClickyActivity.java
@@ -3,6 +3,10 @@ package edu.neu.madcourse.numad21fa_nadiiaramthun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ClickyClickyActivity extends AppCompatActivity {
 
@@ -10,5 +14,67 @@ public class ClickyClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_clicky);
+
+        Button buttonA = (Button) findViewById(R.id.btn_A);
+        Button buttonB = (Button) findViewById(R.id.btn_B);
+        Button buttonC = (Button) findViewById(R.id.btn_C);
+        Button buttonD = (Button) findViewById(R.id.btn_D);
+        Button buttonE = (Button) findViewById(R.id.btn_E);
+        Button buttonF = (Button) findViewById(R.id.btn_F);
+
+        View.OnTouchListener onTouchListener = new MyTouchListener();
+        buttonA.setOnTouchListener(onTouchListener);
+        buttonB.setOnTouchListener(onTouchListener);
+        buttonC.setOnTouchListener(onTouchListener);
+        buttonD.setOnTouchListener(onTouchListener);
+        buttonE.setOnTouchListener(onTouchListener);
+        buttonF.setOnTouchListener(onTouchListener);
     }
+
+    protected class MyTouchListener implements View.OnTouchListener {
+
+        public boolean onTouch(View view, MotionEvent event) {
+            switch(view.getId()){
+                case R.id.btn_A: {
+                    handleButtonTouched(R.string.pressedA, event);
+                    break;
+                }
+                case R.id.btn_B: {
+                    handleButtonTouched(R.string.pressedB, event);
+                    break;
+                }
+                case R.id.btn_C: {
+                    handleButtonTouched(R.string.pressedC, event);
+                    break;
+                }
+                case R.id.btn_D: {
+                    handleButtonTouched(R.string.pressedD, event);
+                    break;
+                }
+                case R.id.btn_E: {
+                    handleButtonTouched(R.string.pressedE, event);
+                    break;
+                }
+                case R.id.btn_F: {
+                    handleButtonTouched(R.string.pressedF, event);
+                    break;
+                }
+            }
+            return false;
+        }
+
+        private void handleButtonTouched(int pressedLetter, MotionEvent event) {
+            TextView textView = (TextView) findViewById(R.id.txt_info_pressed);
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    textView.setText(pressedLetter);
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    textView.setText(R.string.pressed);
+                    break;
+            }
+        }
+    }
+
 }
