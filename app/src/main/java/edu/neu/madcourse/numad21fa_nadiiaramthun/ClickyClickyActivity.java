6dbf09740926893ff9f60214cc7e28f3dd3c6dbe
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-09-25 08:55:31 -0700

    Added new activity

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/ClickyClickyActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21fa_nadiiaramthun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyClickyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+    }
+}
