ac062df03f85b23fba61ab73fdb4e8fb51aab773
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-10-03 15:06:03 -0700

    Added link collector activity

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/LinkCollectorActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21fa_nadiiaramthun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinkCollectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link_collector);
+    }
+}
