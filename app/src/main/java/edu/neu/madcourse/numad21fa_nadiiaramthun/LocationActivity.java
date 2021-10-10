57aefb176a2f001969089009f78225e03b8fc024
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-10-10 10:25:38 -0700

    Initial work on granting runtime access

--- app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/LocationActivity.java
@@ -1,8 +1,15 @@
 package edu.neu.madcourse.numad21fa_nadiiaramthun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            String msg;
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                msg = "Permission granted";
+            } else {
+                msg = "Permission denied";
+            }
+
+            Snackbar.make(findViewById(android.R.id.content), msg, Snackbar.LENGTH_LONG).show();
+        }
     }
 }
