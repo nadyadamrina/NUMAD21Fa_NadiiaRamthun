9fad1072fbff869fa6a2395fbc9b90b537944ba3
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-10-24 09:35:36 -0700

    Added data model for API data

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_nadiiaramthun/user/location/Coordinates.java
@@ -0,0 +1,27 @@
+package edu.neu.madcourse.numad21fa_nadiiaramthun.user.location;
+
+public class Coordinates {
+    private String latitude;
+    private String longitude;
+
+    public Coordinates(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
